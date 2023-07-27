0b1b6f2a230d0a4cf229cdc3fe28f16193979c07
unknown <reingsroman042@gmail.com>
2023-07-27 23:24:28 +0600

    setup files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Button, Container, Form, Row, Col } from "react-bootstrap";
+import axios from "axios";
 
 function App() {
+
+  // this is get method 
+
+  const [ fromData, setFormData ] = useState({})
+
+  useEffect(()=>{
+
+    const getBlog = async () =>{
+
+      
+    }
+
+
+  },[])
+
+  // this is post method 
+
+  const submitBlog = async () =>{
+    console.log()
+  }
+
+  // this is delete method 
+
+  const deleteBlog = async () =>{
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="my-5 text-center">Post a Post</h1>
+        <Container>
+          <Row>
+            <Col lg={4} md={6} sm={12} className="my-5">
+
+              <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control name="name" type="text" placeholder="Name" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label>Description</Form.Label>
+                  <Form.Control name="des" type="text" placeholder="Description" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            </Col>
+            <Col lg={8} md={6} sm={12} className="my-5">
+              up comming post 
+            </Col>
+          </Row>
+        </Container>
+
       </header>
     </div>
   );
